5506c3da6e3975186f2665b9bd3e05f048c13041
simtaxx <souici.haris@gmail.com>
2019-05-29 11:49:33 +0200

    form secu

--- connexion.php
+++ connexion.php
@@ -3,22 +3,23 @@
 
   if ($_POST["log"] != null and $_POST["mdp"] != null){
 
-  $log = $_POST['log'];
-  $mdp = $_POST['mdp'];
-  $bdd = new Entity\Bdd();
-  $user = $bdd->getUser($log);
-  $correctPassword = password_verify($mdp, $user->password());
-  if ($correctPassword ) {
+    
+    $log = $_POST['log'];
+    $mdp = $_POST['mdp'];
+    $bdd = new Entity\Bdd();
+    $user = $bdd->getUser($log);
+    $correctPassword = password_verify($mdp, $user->password());
+    if ($correctPassword ) {
 
-    session_start();
-    $_SESSION['user'] = $user ;
+      session_start();
+      $_SESSION['user'] = $user ;
 
-    header('Location: accueil.php');
-    exit();
-  } else {
-    header('Location: index.php');
-    exit();
-  }
+      header('Location: accueil.php');
+      exit();
+    } else {
+      header('Location: index.php');
+      exit();
+    }
 } else {
   header('Location: index.php');
   exit();
--- createTopics.php
+++ createTopics.php
@@ -8,25 +8,35 @@ if (!isset($_POST['login_post'])) {
     header('Location: topicsCreate.php?message=' . $message);
     exit();
 } else {
-  if (empty($_POST['title'])) {
+  $title = $_POST['title'];
+  $content = $_POST['content_post'];
+  function removeslashes($string)
+{
+    $string=implode("",explode("\\",$string));
+    return stripslashes(trim($string));
+}
+  $title = removeslashes($title);
+  $content = removeslashes($content);
+  var_dump($title);
+  if (empty($title)) {
     $message = 'titre non rempli!';
     header('Location: topicsCreate.php?message=' . $message);
     exit();
-  } elseif(empty($_POST['content_post'])){
+  } elseif(empty($content)){
     $message = 'contenu non rempli!';
     header('Location: topicsCreate.php?message=' . $message);
     exit();
-  }elseif (strlen($_POST['title']) > 100) {
+  }elseif (strlen($title) > 100) {
     $message = 'titre du topic trop long!';
     header('Location: topicsCreate.php?message=' . $message);
     exit();
   } else {
-    $newTopic = new Entity\Topics('', $_POST['title'], 0, $_SESSION['user']->id());
+    $newTopic = new Entity\Topics('', $title, 0, $_SESSION['user']->id());
     var_dump($newTopic);
     $newTopic->saveBdd();
     $date = date("d-m-Y");
     $lastId = Entity\Bdd::getLastIdTopics();
-    $newPost = new Entity\Post('', $_POST['title'], $_POST['content_post'], $date, $_SESSION['user']->id(), $lastId[0]);
+    $newPost = new Entity\Post('', $title, $content, $date, $_SESSION['user']->id(), $lastId[0]);
     $newPost->saveBdd();
 
     $message = 'Topics bien poster';
