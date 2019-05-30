91b397ef6609699521413358a707a3585a24b71d
SHatice <sarah.gungor@hetic.net>
2019-05-30 15:48:17 +0200

    add home style

--- elements/header.php
+++ elements/header.php
@@ -1,10 +1,9 @@
 <header class="header">
   <div class="logo--container">
-    <img class="logo--cover" src="./assets/img/logo.svg" alt="">
+    <p>Youtaites</p>
   </div>
   <nav>
     <ul class="nav--list">
-      <li class="nav--item"><a href="./deconnexion.php">Déconnexion</a></li>
       <li class="nav--item"><a href="./accueil.php">Accueil</a></li>
       <li class="nav--item">Recrutement</li>
       <li class="nav--item">Tutoriel</li>
@@ -13,9 +12,9 @@
   </nav>
   <div class="btn--container">
     <button class="btn"><a href="./topicsCreate.php">Nouveau Post</a></button>
+    <!-- <div><a href="./deconnexion.php">Déconnexion</a></div> -->
   </div>
   <div class="userImg--container">
-
     <img class="userImg--cover" src="<?php echo $_SESSION['user']->picture(); ?>" alt="">
   </div>
 </header>
